12e4120af357b046695f3bcbf3bdb8cac2666912
moneya <moneya59@gmail.com>
2019-11-20 13:09:34 +0100

    fixed some other pages

--- resources/views/files/file_upload.blade.php
+++ resources/views/files/file_upload.blade.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-        <!--responsiveness-->
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!-- Styles -->
-        <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link â€Žhref="https://fonts.googleapis.com/css?family=europa:200600" rel="stylesheet">
-<title>upload files</title>
-</head>
-<body>
-@include('layouts.sidebar')
-<div>
-Files
-</div>
+@extends('layouts.sidbar')
+@section('content')
 <div class="fa fa-search">
         <input type="text" name="search" placeholder="Search" class="search" />
         <img src="{{ asset('css/icons/search.png') }}" alt="" />
@@ -41,5 +23,5 @@ Files
                 <td>eeee</td>
               </tbody>
             </table>
-</body>
-</html>
+@stop
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,33 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
-        <!-- Styles -->
-        <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link â€Žhref="https://fonts.googleapis.com/css?family=europa:200600" rel="stylesheet">
-        <!--responsiveness-->
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>echoCV</title>
-</head>
-<body>
-
-<div class="wrapper">
-
-
-        @include('layouts.sidebar')
-
-
-    <!-- Page Content -->
-    <section class="onboard">
+@extends('layouts.empty-sidbar')
+  @section('content')
 
             <div class="col-md-10 mt-5 mb-5 p-5">
+
                <div class="empty text-center">
                  <img src="{{ asset('css/icons/vector.png') }}" class="mx-auto d-block" />
                   <h3>Build your Dashboard</h3>
@@ -37,33 +12,4 @@
                </div>
             </div>
 
-    </section>
-
-</div>       
-
-
-{{--           <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <!-- Popper.JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-    <!-- jQuery Custom Scroller CDN -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script> --}}
-{{-- 
-    <script>
-      $(document).ready(function () {
-
-    $(".sidebar").mCustomScrollbar({
-         theme: "minimal"
-    });
-
-    $('.sidebarCollapse').on('click', function () {
-        $('.sidebar').toggleClass('active');
-    });
-
-});
-    </script> --}}
-
-</body>
-</html>
+@stop
