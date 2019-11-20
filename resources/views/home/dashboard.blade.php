8131fad470508938c89a562946e776a9eb193465
moneya <moneya59@gmail.com>
2019-11-20 14:29:05 +0100

    fixed dashboard

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
-        <!-- Styles -->
-        <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link Ã¢â‚¬Å½href="https://fonts.googleapis.com/css?family=europa:200600" rel="stylesheet">
-        <!--responsiveness-->
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-        <title>echoCV</title>
-</head>
-<body>
-@include('layouts.sidebar')
-<section class="onboard">
-        <div class="new">
-          <img src="{{ asset('css/icons/vector.png') }}" />
-          <h3>Build your Dashboard</h3>
-          <p>Put together a personal dashboard consisting of metrics that matter to you</p>
-         <a href="/add_chart"> <button type="button" name="button" class="btn btn-primary">Create dashboard</button></a>
-          </div>
-      </section>
-</body>
-</html>
+@extends('layouts.empty-sidbar')
+  @section('content')
+
+            <div class="col-md-10 mt-5 mb-5 p-5">
+
+               <div class="empty text-center">
+                 <img src="{{ asset('css/icons/vector.png') }}" class="mx-auto d-block" />
+                  <h3>Build your Dashboard</h3>
+                    <p>Put together a personal dashboard consisting of metrics that matter to you</p>
+                     <a class="btn btn-primary" href="/add_chart">Create dashboard</a>
+               </div>
+            </div>
+
+@stop
