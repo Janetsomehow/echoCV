be9e4ebdfdb90f2af59f053f51eaa3eac4e901b8
Agboola Eniola <agboola.eniola4031@gmail.com>
2019-11-22 03:38:25 +0100

    Built add company form

--- resources/views/portfolio_company/add_company.blade.php
+++ resources/views/portfolio_company/add_company.blade.php
@@ -1,44 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
-        <!-- Styles -->
-        <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link Ã¢â‚¬Å½href="https://fonts.googleapis.com/css?family=europa:200600" rel="stylesheet">
-        <!--responsiveness-->
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>echoCV</title>
-</head>
-<body>
-@include('layouts.sidebar')
+
+  @extends('layouts.empty-sidbar')
+  @section('content')
+
+
 <div class="tag">Add Company</div>
+<div class="onobard>
 <form action="" method="post">
             <div class="form-group">
-              <label for="company">Select spreadsheets</label>
-              <select class="form-control form-group" name="company">
-                <option value=""></option>
-                <option value="First">First</option>
-                <option value="Second">Second</option>
-                <option value="Third">Third</option>
-              </select>
-              <label for="">Select sheets</label>
-              <select class="form-control" name="">
-                <option value=""></option>
-                <option value="First">First</option>
-                <option value="Second">Second</option>
-                <option value="Third">Third</option>
-              </select>
+              <label for="name">Company Name</label>
+    <input type="text" class="form-control" id="companyname">
+    <label for="web">Company Website</label>
+    <input type="text" class="form-control" id="companyweb">
               <div class="bigSe">
                 <div class="">
-                  <label for="">Select rows(date and time period)</label>
+                  <label for="country">Country</label>
                   <select class="form-control" name="">
+                  <option selected>Select Country</option>
                     <option value=""></option>
                     <option value="First">First</option>
                     <option value="Second">Second</option>
@@ -46,29 +23,50 @@
                   </select>
                 </div>
                 <div class="">
-                  <label for="">Select metrics column (name)</label>
+                  <label for="">Primary Contact</label>
                   <select class="form-control" name="">
+                  <option selected>Select Contact</option>
                     <option value=""></option>
                     <option value="First">First</option>
                     <option value="Second">Second</option>
                     <option value="Third">Third</option>
                   </select>
                 </div>
-                
-              </div>
-              <div class"form-row">
+
+              <label for="tags">Tags</label>
+    <input type="text" class="form-control" id="companyname">
+              <div class="form-row">
    <div class="col-6">
-       <label>First Name<\label><br>
+       <label for="First Name">Fund Stage</label><br>
+       <select class="form-control" name="">
+                    <option value=""></option>
+                    <option value="First">First</option>
+                    <option value="Second">Second</option>
+                    <option value="Third">Third</option>
+                  </select>   </div>
+   
+    <div class="col-6">
+       <label for="Last Name">Investment Status</label><br>
+       <select class="form-control" name="">
+                    <option value=""></option>
+                    <option value="First">First</option>
+                    <option value="Second">Second</option>
+                    <option value="Third">Third</option>
+                  </select>   </div>
+   <div class="col-6">
+       <label for="First Name">Lead</label><br>
        <input type="text" class="form-control">
    </div>
    
     <div class="col-6">
-       <label>Last Name<\label><br>
+       <label for="Last Name">Analyst</label><br>
        <input type="text" class="form-control">
    </div>
-<\div>
+
             </div>
             <input type="submit" name="submit" value="Save" class="btn btn-primary btns">
             <input type="submit" name="submit" value="Cancel" class="btn btn-default btns btnCancel">
-
+            </div>
           </form>
+</div>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,20 @@
 <html>
     <head><title>landing page</title></head>
     <body>
+        <div>
         <div class="mask">
             <img src="{{ asset('css/icons/land.png') }}" style="height: 150%; width: 165%;"/>
 </div>
 <div class="echo">
-<img src="{{ asset('css/icons/echoVC 1.png')}}"  style="height: 48%; width: 25%;"/>
+<img src="{{ asset('css/icons/echoVC 1.png')}}" style="height: 47%; width: 46%;"/>
+</div>
+<div class="echotxt">
 <p>Portfolio Management System</p>
 <p>(WEB)</p>
 </div>
+</div>
 <style>
     body{
-        max-width:100%;
         background-color:#0E0E0E;
     }
 .mask{
@@ -23,18 +26,23 @@
 }
 .echo{
     position:absolute;
-    left:16%;
+    left:25%;
     bottom:30%;
     top:35%;
+    width:45%;
+    height:35%;
     }
-    p{
+    .echotxt{
+        position:absolute;
         color: #FFFFFF;
-        font-family: Futura PT;
-        font-weight: normal;
-        font-style: normal;
+        font-family:Futura PT;
         letter-spacing: 0.05em;
-        font-size: 160%;
-        line-height: 20px;
+        font-size: 155%;
+        line-height: 21%;
+        text-align:center;
+        bottom:30%;
+        left:22%;
+top:52%;
     }
     </style>
 </body>
