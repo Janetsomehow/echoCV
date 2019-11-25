7973b1e01d59f2871f3d36d36038bce9e22bfbdc
Agboola Eniola <agboola.eniola4031@gmail.com>
2019-11-25 13:59:38 +0100

    added responsiveness to company page

--- resources/views/home/add_chart.blade.php
+++ resources/views/home/add_chart.blade.php
@@ -3,11 +3,45 @@
 @section('content')
 <div class="">
 <h3 class="tag">Welcome back, Eniola Agboola</h3>
-<button type="button" name="chart" class="btn btn-primary">Add chart</button>
-<button type="button" name="share" class="btn btn-primary">share</button>
+<a href="#">Fund 1</a>
+<a href="#">Fund </a>
 </div>
-<h5>Fund 1</h5>
-<h5>Fund </h5>
+<div>
+<button type="button" name="chart" class="btn btn-primary">Add chart</button>
+<button type="button" name="share" class="btn">share</button>
+</div>
 <hr>
-
+<p>Investor Relations</p>
+<div>
+<!--<table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>-->
+</div>
 @stop
--- resources/views/portfolio_company/add_company.blade.php
+++ resources/views/portfolio_company/add_company.blade.php
@@ -1,18 +1,19 @@
 
-  @extends('layouts.empty-sidbar')
-  @section('content')
-
-
-<div class="tag">Add Company</div>
-<div class="onobard>
+@extends('layouts.empty-sidbar')
+ @section('content')
+ <div class="header">
+<div class="d-flex">
+<div class="p-2"><h4>Add Company</h4></div>
+<div class="mt-4">
+  <div class="row justify-content-center">
+  <div class="col-md-8">
 <form action="" method="post">
-            <div class="form-group">
               <label for="name">Company Name</label>
     <input type="text" class="form-control" id="companyname">
     <label for="web">Company Website</label>
     <input type="text" class="form-control" id="companyweb">
               <div class="bigSe">
-                <div class="">
+                <div>
                   <label for="country">Country</label>
                   <select class="form-control" name="">
                   <option selected>Select Country</option>
@@ -22,9 +23,9 @@
                     <option value="Third">Third</option>
                   </select>
                 </div>
-                <div class="">
-                  <label for="">Primary Contact</label>
-                  <select class="form-control" name="">
+                <div>
+                  <label for="pcontact">Primary Contact</label>
+                  <select class="form-control" name="pcontact">
                   <option selected>Select Contact</option>
                     <option value=""></option>
                     <option value="First">First</option>
@@ -34,7 +35,7 @@
                 </div>
 
               <label for="tags">Tags</label>
-    <input type="text" class="form-control" id="companyname">
+    <input type="text" class="form-control" id="companyname" placeholder="type in one or more tags to select"><br>
               <div class="form-row">
    <div class="col-6">
        <label for="First Name">Fund Stage</label><br>
@@ -52,7 +53,7 @@
                     <option value="First">First</option>
                     <option value="Second">Second</option>
                     <option value="Third">Third</option>
-                  </select>   </div>
+                  </select><br>   </div>
    <div class="col-6">
        <label for="First Name">Lead</label><br>
        <input type="text" class="form-control">
@@ -60,13 +61,16 @@
    
     <div class="col-6">
        <label for="Last Name">Analyst</label><br>
-       <input type="text" class="form-control">
+       <input type="text" class="form-control"><br>
    </div>
-
-            </div>
-            <input type="submit" name="submit" value="Save" class="btn btn-primary btns">
+               <input type="submit" name="submit" value="Save" class="btn btn-primary btns">
             <input type="submit" name="submit" value="Cancel" class="btn btn-default btns btnCancel">
-            </div>
-          </form>
+</div>
+          </div>
+</form>
+</div>
+</div>
+</div>
+</div>
 </div>
 @stop
