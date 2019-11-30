797a8c7f037e83aef3584588f03a80cfde36c32f
Janetsomehow <--global>
2019-11-30 10:14:37 +0100

    Revert "Made some changes"
    This reverts commit 5627262c2a35a18e0478e5d3cec0e9bc9ff43603.

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.empty-sidbar')
   @section('content')
 
-            <div class="col-md-10 mt-5 mb-5 p-5 wrapper">
+            <div class="col-md-10 mt-5 mb-5 p-5">
 
                <div class="empty text-center">
                  <img src="{{ asset('css/icons/vector.png') }}" class="mx-auto d-block" />
-                  <h3 class="text-wrap">Build your Dashboard</h3>
-                    <p class="text-wrap">Put together a personal dashboard consisting of metrics that matter to you</p>
-                     <button type="button" class="btn btn-primary" href="/add_chart">Create dashboard</button>
+                  <h3>Build your Dashboard</h3>
+                    <p>Put together a personal dashboard consisting of metrics that matter to you</p>
+                     <a class="btn btn-primary" href="/add_chart">Create dashboard</a>
                </div>
             </div>
 
