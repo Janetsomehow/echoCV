aa38037e6bfdb6a9687225e77d01dd7f4ebe4208
Agboola Eniola <agboola.eniola4031@gmail.com>
2019-12-05 13:27:16 +0100

    added home chart

--- resources/views/home/add_chart1.blade.php
+++ resources/views/home/add_chart1.blade.php
@@ -1,22 +1,22 @@
-@extends('layouts.sidbar')
-
 @section('content')
-<div class="grid-container"> 
- <div class="col-lg">
+<div class="container">
+    <div class="row"> 
+            <div class="col-8" style="border:2px; background-color:red; min-height:800px;">
  <canvas id="barchart"></canvas>
- </div>
+            </div>
 
-<div class="col-sm-4">
-     <div>
+            <div class="col-4" style="background-color:blue; min-height:800px;">
+        <div>
      <ul class="list-inline">
     <li><a data-toggle="pill" href="#">metrics</a></li>
     <li><a  data-toggle="pill" href="#">export</a></li>
   </ul>
 <a hre="#">Cancel</a>
   <button class="btn btn-primary">save</button>
-</div>
+        </div>
      <form>
 </form>
-</div>
+            </div>
+    </div>
 </div>
  @stop
