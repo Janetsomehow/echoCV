1f349a126288d9b7a5b30e3e259af43875a1e1f7
Agboola Eniola <agboola.eniola4031@gmail.com>
2019-12-09 21:22:40 +0100

    BS modal

--- resources/views/layouts/sidbar.blade.php
+++ resources/views/layouts/sidbar.blade.php
@@ -50,6 +50,7 @@
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <!-- Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 
