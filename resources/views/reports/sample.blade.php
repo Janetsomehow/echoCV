ca127d71dc01ea301cc03ecf71cb7fa2696f851d
JanetSomehow <janetsomehow@gmail.com>
2020-01-24 18:16:21 +0100

    Created sample report page

--- resources/views/reports/sample.blade.php
+++ resources/views/reports/sample.blade.php
@@ -7,6 +7,7 @@
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
       <!-- Styles -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
       <link href="{{ asset('css/report.css') }}" rel="stylesheet">
       <link href="{{ asset('css/sample.css') }}" rel="stylesheet">
@@ -24,91 +25,172 @@
     </div>
     <main class="wholeContent">
 
-      <section class="">
-        <div class="heading container mt-4 ml-4">
-          <h3> August Investor Update</h3>
-          <p> To: <span class="text-info"> Jane Magnesys for Netflix</span></p>
+      <section class="page">
+        <div class="desktopView  col-md-6 mx-auto">
+          <div class="heading container mt-4 ">
+            <h3> August Investor Update</h3>
+            <p class="not-mobile-content"> To: <span class="text-info "> Jane Magnesys for Netflix</span></p>
+            
+          </div>
+          <hr class="shadow not-mobile-content">
+
+          <div class="container sample-content">
+            <p class="text-wrap">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Earum asperiores eos adipisci at, cum magnam repellendus, 
+              aliquid perferendis ad explicabo molestiae nulla deserunt? 
+              Autem ipsam labore nihil nam accusantium maxime.
+
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Earum asperiores eos adipisci at, cum magnam repellendus, 
+              aliquid perferendis ad explicabo molestiae nulla deserunt? 
+              Autem ipsam labore nihil nam accusantium maxime.
+
+            </p>
+          </div>
+
+          <div class="container">
+            <img src="{{ asset('css/icons/sample.png') }}" class="img-fluid" alt="">
+          </div>
+        
+          <div class="thanks container mt-4">
+            <h4> Thanks</h4>
+            <p class="text-wrap">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi optio facere accusantium
+              neque consectetur ea incidunt perspiciatis est. Necessitatibus perspiciatis aliquid eaque
+                sapiente asperiores consequuntur tempora quia voluptatum earum fuga.
+            </p>
+          </div>
+          <hr class="shadow">
+          <div class="not-mobile-content">
+            <div class="container row recieved ">
+              <h5 class="ml-3"> Recieved</h5>
+              <h6 class="ml-auto">Year: 
+              <select name="" id="">
+                <option value=""> 2019</option>
+                <option value=""> 2019</option>
+              </select>
+              </h6>
+            </div>
+            <div class="scrolling-wrapper  container">
+              <div class="cal ml-0"><p> Sep 2019</p></div>
+              <div class="cal "><p> Jan 2019</p></div>
+              <div class="cal "><p> Feb 2019</p></div>
+              <div class="cal "><p> Mar 2019</p></div>
+              <div class="cal"><p> Apr 2019</p></div>
+              <div class="cal"><p> May 2019</p></div>
+              <div class="cal"><p> Jun 2019</p></div>
+              <div class="cal"><p> Jul 2019</p></div>
+              <div class="cal"><p> Aug 2019</p></div>
+              <div class="cal"><p> Sep 2019</p></div>
+              <div class="cal"><p> Oct 2019</p></div>
+              <div class="cal"><p> Nov 2019</p></div>
+              <div class="cal"><p> Dec 2019</p></div>
+            </div>
+
+            <div class="container  row">
+              <div class="sent ml-3">
+                <p> Not sent</p>
+                <p class="text-info"> Send</p>
+              </div>
+
+              <div class="view ml-5">
+                <p> View</p>
+              </div>
+
+              <div class="view ml-4">
+                <p> View</p>
+              </div>
+
+            
+            </div>
+
+            <div class="walk container ml-2">
+              <div class="row">
+                <p class="mr-auto">Number of Walkins</p>
+                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Number
+                </button>
+                <input type="text" placeholder="Enter Value" class="p-2 ml-auto mr-4 form-control" style="width: 6em">
+              </div>
+              <div class="row mt-5">
+                <p class="mr-auto">Total Net Profit</p>
+                <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Currency
+                </button>
+                <input type="text" placeholder="Enter Value" class="p-2 ml-auto mr-4 form-control" style="width: 6em; ">
+              </div>
+            </div>
+
+            <div class="pitch container mt-5">
+              <h3> Pitch deck</h3>
+              <p class="text-wrap"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Cumque non veritatis iusto quo recusandae asperiores esse 
+                libero doloremque aperiam mollitia amet, quis culpa tempora
+                  unde veniam. Eum veritatis magni harum!</p>
+              <button class="btn btn-secondary"> Upload file</button>
+            </div>
+
+            
+        
+        </div>
+        <div class="container ">
+          <div class="col">
+            <div class="row">
+                <p> Section title</p>
+                <p class="ml-auto"> Required </p>
+            </div>
+            <div class="form-group align-form  ">
+              <input type="text" placeholder="Monthly metrics" class="p-3 form-control">
+            </div>
+
+            <div class="form-group align-form">
+              <p> Description</p>
+              <textarea name="" id="" class="form-control text-wrap" placeholder="dfdf" >
+                
+              </textarea>
+            </div>
+
+            <div class="kpi headed row align-form">
+              <div class=" col-elements">
+                 <p>KPI name</p>
+                 <input type="text" class="form-control " placeholder="Number of Walk-Ins" >
+                 <input type="text" class="form-control mt-4" placeholder="Total Net Profit" >
+              </div>
+
+              <div class="mx-auto col"> 
+                <p>Format</p>
+                <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+                  Number
+                </button>
+
+                <button class="btn btn-secondary  dropdown-toggle btn-sm" style="margin-top: 2.6em" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+                  Currency
+                </button>
+              </div>
+
+              <div class="ml-auto text-right"> 
+                <p class="">KPI Value</p>
+                <input type="text" class="form-control " placeholder="Enter Value" >
+                <input type="text" class="form-control mt-4" placeholder="Enter Value" >
+              </div>
+              
+              <div class="base-kpi row ml-n4 mt-3">
+                <p class="text-info"> <i class="fas fa-plus"></i> Add KPI</p>
+                 <i class="fas fa-trash-alt"></i>
+              </div>
+            </div>
+
+            
+          </div>
+          
           
         </div>
-        <hr class="shadow">
 
-        <div class="container sample-content">
-          <p class="text-wrap">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Earum asperiores eos adipisci at, cum magnam repellendus, 
-            aliquid perferendis ad explicabo molestiae nulla deserunt? 
-            Autem ipsam labore nihil nam accusantium maxime.
+      </div>
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Earum asperiores eos adipisci at, cum magnam repellendus, 
-            aliquid perferendis ad explicabo molestiae nulla deserunt? 
-            Autem ipsam labore nihil nam accusantium maxime.
-
-          </p>
-        </div>
-
-        <div class="container">
-          <img src="{{ asset('css/icons/sample.png') }}" class="img-fluid" alt="">
-        </div>
-       
-        <div class="thanks container mt-4">
-          <h4> Thanks</h4>
-          <p class="text-wrap">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi optio facere accusantium
-             neque consectetur ea incidunt perspiciatis est. Necessitatibus perspiciatis aliquid eaque
-              sapiente asperiores consequuntur tempora quia voluptatum earum fuga.
-          </p>
-        </div>
-        <hr class="shadow">
-
-        <div class="container row recieved">
-          <h5 class="ml-3"> Recieved</h5>
-          <h6 class="ml-auto">Year: 
-          <select name="" id="">
-            <option value=""> 2019</option>
-            <option value=""> 2019</option>
-          </select>
-          </h6>
-        </div>
-        <div class="scrolling-wrapper  container">
-          <div class="cal ml-0"><p> Sep 2019</p></div>
-          <div class="cal "><p> Jan 2019</p></div>
-          <div class="cal "><p> Feb 2019</p></div>
-          <div class="cal "><p> Mar 2019</p></div>
-          <div class="cal"><p> Apr 2019</p></div>
-          <div class="cal"><p> May 2019</p></div>
-          <div class="cal"><p> Jun 2019</p></div>
-          <div class="cal"><p> Jul 2019</p></div>
-          <div class="cal"><p> Aug 2019</p></div>
-          <div class="cal"><p> Sep 2019</p></div>
-          <div class="cal"><p> Oct 2019</p></div>
-          <div class="cal"><p> Nov 2019</p></div>
-          <div class="cal"><p> Dec 2019</p></div>
-        </div>
-
-        <div class="container  row">
-          <div class="sent ml-3">
-            <p> Not sent</p>
-            <p class="text-info"> Send</p>
-          </div>
-
-          <div class="view ml-5">
-            <p> View</p>
-          </div>
-
-          <div class="view ml-4">
-            <p> View</p>
-          </div>
-
-         
-        </div>
-
-        <div class="container row  calc">
-          <div class="d-flex justify-content-start"> <p> Number of Walk ins</p></div>
-           <p> Number </p>
-           <input type="text" placeholder="Enter Value">
-        </div>
       </section>
+      <div style="height: 10rem"></div>
     </main>
     </body>
 </html>
