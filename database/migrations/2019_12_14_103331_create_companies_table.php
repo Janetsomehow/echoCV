8b4b13c48d4bee908cf9277434d8f2f20132d306
eniola4031 <agboola.eniola4031@gmail.com>
2020-02-05 14:42:18 +0100

    foreign keys

--- database/migrations/2019_12_14_103331_create_companies_table.php
+++ database/migrations/2019_12_14_103331_create_companies_table.php
@@ -8,14 +8,14 @@ class CreateCompaniesTable extends Migration {
 	public function up()
 	{
 		Schema::create('companies', function(Blueprint $table) {
-		$table->engine = 'MYISAM';
+		//$table->engine = 'MYISAM';
 			$table->increments('id');
 			$table->uuid('uuid')->nullable();
 			$table->string('c_name');
 			$table->string('website')->nullable();
 			$table->bigInteger('user_id')->unsigned()->nullable();
-			$table->string('country')->unsigned();
-			$table->string('contact_id')->unsigned();
+			$table->string('country');
+			$table->bigInteger('contact_id')->unsigned();
 			$table->enum('status', array('open', 'close'))->nullable();
 			$table->enum('stage', array('seed', 'seriesA', 'seriesB', 'seriesC'))->nullable();
 			$table->string('lead')->nullable();
