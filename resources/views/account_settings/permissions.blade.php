9023a59e07992e805e73d3acffcbcaa8d6dcc4b7
Janetsomehow <janetsomehow@gmail.com>
2020-02-07 13:30:01 +0100

    Made some changes

--- resources/views/account_settings/permissions.blade.php
+++ resources/views/account_settings/permissions.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Account Settings</title>
+    
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     {{-- <link ‎href="https://fonts.adobe.com/fonts/europa" rel="stylesheet"> --}}
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,600" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/report.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/account.css') }}" rel="stylesheet">
 
@@ -27,56 +29,27 @@
                 <aside>
                     <div class="list-group list-group-flush permission-right">
                             <a href="/profile" class="list-group-item list-group-item-action permission " style="color: #333333"> My profile</a></li>
-                            <a href="#" class="list-group-item list-group-item-action border-none permission" style="color: #333333"> Permissions</a></li>
+                            <a href="#" class="list-group-item list-group-item-action border-none permission p-2" style="color: #333333"> Users and Permissions</a></li>
                     </ul>
                 </aside>
             </div>
             <div class="profile-form">
-                    <h4 class="font-weight-bold"> Permissions</h4>
+                    <h4 class="font-weight-bold">Users and Permissions</h4>
 
-                    <h5 class="mt-4 font-weight-bold"> New Users </h5>
-
-                    <div class="selecting"> 
+                    <form class="form-inline form-search mt-4" style="border-radius: 4px"  >
+                        <label class="sr-only" for="inlineFormInputName2"></label>
+                        <input type="search" class="form-control search-form mb-2   mr-sm-2" style="width: 24rem"="inlineFormInputName2" placeholder="Search for a user">
                       
-                        <h6> Edit </h6>
-                        <div class="row">
-                            <p class="col-9">
-                                    Can view dashboards, portfolio company data, upload files and cannot manage users
-                            </p>
-                            <label for="checkbox"> </label>
-                            <input type="checkbox" class="m-1 mb-2 d-inline regular-checkbox "  name=""  checked id="">
-                            <span class="checkmark">
-                            </span>
-                        
-                        </div>
-
-                        <h6> Collaborate </h6>
-                        <div class="row">
-                            <p class="col-9">
-                                    Can create and edit shared assets like dashboards and portfolio company data
-                            </p>
-                            <label for="checkbox"> </label>
-                                <input type="checkbox" class="m-1 mb-2 d-inline regular-checkbox "  name="" disabled checked id="">
-                                <span class="checkmark">
-                                </span>
-                            
-                        </div>
-                        
-                        <h6> Manage Users </h6>
-                        <div class="row">
-                            <p class="col-9">
-                                    Can create and edit shared assets like dashboards and portfolio company data
-                            </p>
-                            <label for="checkbox"> </label>
-                            <input type="checkbox" class="m-1 mb-2 d-inline regular-checkbox "  name="" disabled checked id="">
-                            <span class="checkmark">
-                            </span>
-                        </div>
-
-                        <div class="manage mt-5">
-                            <h4 class="font-weight-bold"> Manage users </h4>
-                            <div class="table ">
-                                <table class=" table-responsive table-borderless table-hover" width="700" style="margin-top: -4rem;">
+                        <button type="submit" class="btn btn-primary search-btn btn-sm mb-2 p-2">
+                          <svg class="svg" width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M15.0833 13.3333H14.1617L13.835 13.0183C15.0179 11.6464 15.668 9.89481 15.6667 8.08334C15.6667 6.58349 15.2219 5.11734 14.3886 3.87026C13.5554 2.62319 12.371 1.65121 10.9854 1.07725C9.59968 0.503286 8.07492 0.35311 6.6039 0.645715C5.13288 0.93832 3.78166 1.66056 2.72111 2.72111C1.66056 3.78166 0.93832 5.13288 0.645715 6.6039C0.35311 8.07492 0.503286 9.59968 1.07725 10.9854C1.65121 12.371 2.62319 13.5554 3.87026 14.3886C5.11734 15.2219 6.58349 15.6667 8.08334 15.6667C9.96167 15.6667 11.6883 14.9783 13.0183 13.835L13.3333 14.1617V15.0833L19.1667 20.905L20.905 19.1667L15.0833 13.3333ZM8.08334 13.3333C5.17834 13.3333 2.83334 10.9883 2.83334 8.08334C2.83334 5.17834 5.17834 2.83334 8.08334 2.83334C10.9883 2.83334 13.3333 5.17834 13.3333 8.08334C13.3333 10.9883 10.9883 13.3333 8.08334 13.3333Z" fill="white"/>
+                          </svg>
+                        </button>
+                
+                    </form>
+                        <div class="manage">
+                            <div class="table ml-1">
+                                <table class=" table-responsive table-borderless table-hover"  style="margin-top: -4rem;">
                                     
                                     <thead >
                                         <tr>
@@ -100,10 +73,19 @@
                                             <td></td>
                                             <td>
                                                 <div class="form-group">
-                                                        <select class="form-control" id="exampleFormControlSelect1">
-                                                            <option>can view</option>
-                                                            <option>can edit</option>
-                                                        </select>
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-default">can view</button>
+                                                        <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                          {{-- <span class="sr-only">Toggle Dropdown</span> --}}
+                                                        </button>
+                                                        <div class="dropdown-menu text-left" aria-labelledby="dropdownMenuReference">
+                                                          <a class="dropdown-item" href="#">can view</a>
+                                                          <a class="dropdown-item" href="#">can edit</a>
+                                                          <a class="dropdown-item" href="#">admin</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item text-danger" href="#">remove</a>
+                                                        </div>
+                                                      </div>
                                                 </div>    
                                             </td>
                                         </tr>
@@ -116,29 +98,98 @@
                                             <td></td>
                                             <td>
                                                 <div class="form-group">
-                                                    <select class="form-control" id="exampleFormControlSelect1">
-                                                        <option>can view</option>
-                                                        <option>can edit</option>
-                                                    </select>
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-default">can view</button>
+                                                        <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                          {{-- <span class="sr-only">Toggle Dropdown</span> --}}
+                                                        </button>
+                                                        <div class="dropdown-menu text-left" aria-labelledby="dropdownMenuReference">
+                                                          <a class="dropdown-item" href="#">can view</a>
+                                                          <a class="dropdown-item" href="#">can edit</a>
+                                                          <a class="dropdown-item" href="#">admin</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item text-danger" href="#">remove</a>
+                                                        </div>
+                                                      </div>
                                                 </div>       
                                             </td>
                                         </tr>
 
                                         <tr >
                                             {{-- <th scope="row">1</th> --}}
-                                            <td colspan="3">Anne Thompson</td>
+                                            <td colspan="3">John Samuel</td>
                                             <td></td>
-                                            <td colspan="3">annethompson@echocv.com</td>
+                                            <td colspan="3">john.samuel@echocv.com</td>
                                             <td></td>
-                                            <td> 
+                                            <td>
                                                 <div class="form-group">
-                                                    <select class="form-control" id="exampleFormControlSelect1">
-                                                        <option>can view</option>
-                                                        <option>can edit</option>
-                                                    </select>
-                                                </div>    
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-default">can view</button>
+                                                        <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                          {{-- <span class="sr-only">Toggle Dropdown</span> --}}
+                                                        </button>
+                                                        <div class="dropdown-menu text-left" aria-labelledby="dropdownMenuReference">
+                                                          <a class="dropdown-item" href="#">can view</a>
+                                                          <a class="dropdown-item" href="#">can edit</a>
+                                                          <a class="dropdown-item" href="#">admin</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item text-danger" href="#">remove</a>
+                                                        </div>
+                                                      </div>
+                                                </div>       
                                             </td>
                                         </tr>
+
+                                        <tr >
+                                            {{-- <th scope="row">1</th> --}}
+                                            <td colspan="3">John Samuel</td>
+                                            <td></td>
+                                            <td colspan="3">john.samuel@echocv.com</td>
+                                            <td></td>
+                                            <td>
+                                                <div class="form-group">
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-default">can view</button>
+                                                        <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                          {{-- <span class="sr-only">Toggle Dropdown</span> --}}
+                                                        </button>
+                                                        <div class="dropdown-menu text-left" aria-labelledby="dropdownMenuReference">
+                                                          <a class="dropdown-item" href="#">can view</a>
+                                                          <a class="dropdown-item" href="#">can edit</a>
+                                                          <a class="dropdown-item" href="#">admin</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item text-danger" href="#">remove</a>
+                                                        </div>
+                                                      </div>
+                                                </div>       
+                                            </td>
+                                        </tr>
+
+                                        <tr >
+                                            {{-- <th scope="row">1</th> --}}
+                                            <td colspan="3">John Samuel</td>
+                                            <td></td>
+                                            <td colspan="3">john.samuel@echocv.com</td>
+                                            <td></td>
+                                            <td>
+                                                <div class="form-group">
+                                                    <div class="btn-group">
+                                                        <button type="button" class="btn btn-default">can view</button>
+                                                        <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                          {{-- <span class="sr-only">Toggle Dropdown</span> --}}
+                                                        </button>
+                                                        <div class="dropdown-menu text-left" aria-labelledby="dropdownMenuReference">
+                                                          <a class="dropdown-item" href="#">can view</a>
+                                                          <a class="dropdown-item" href="#">can edit</a>
+                                                          <a class="dropdown-item" href="#">admin</a>
+                                                          <div class="dropdown-divider"></div>
+                                                          <a class="dropdown-item text-danger" href="#">remove</a>
+                                                        </div>
+                                                      </div>
+                                                </div>       
+                                            </td>
+                                        </tr>
+
                                     </tbody>
                                 </table>
                             </div>
--- resources/views/home/dashboard1.blade.php
+++ resources/views/home/dashboard1.blade.php
@@ -7,9 +7,11 @@
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <!-- Styles -->
       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
       <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
+      <link href="{{ asset('css/report.css') }}" rel="stylesheet">
       <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
       
       <!-- Fonts -->
@@ -27,10 +29,125 @@
       @include('layouts.sidebar')
     </div>
     
-    <div class="">
-      <button class="btn btn-primary"> Add Chart</button>
-      <button class="btn btn-primary"> Share</button>
-    </div>
-  
+    
+    <main class="wholeContent mt-2  "> 
+      <div class="dashboard-header float-right" >  
+        <button class="btn btn-primary">Add Chart </button>
+        <button class="btn btn-dashboard ml-2">Share </button>
+      </div>
+      <div class="dashboards mt-4 ml-2 ">
+        
+          <ul class="row list-unstyled ">
+            <li > <a class="activate list-inline-item" > Dashboard 1 </a></li>
+            <li class="ml-3 list-inline-item" > Dashboard 2</li>
+            <li class="ml-3 list-inline-item"> <i class="fas fa-plus"></i> </li>
+          </ul>
+      </div>
+      <hr>
+
+      <div class="body">
+        <div class="col-10 row justify-content-center">
+          <div class="col-5 ml-5  ">
+            <div class=" ml-1 mt-3">
+              <h4>Coca-Cola</h4>
+              <div class="row " style="margin-left: .1em">
+                <p> Quarterly Overview</p>
+                <i class="fas fa-chevron-down ml-auto"> </i>
+              </div>
+            </div>
+            <div>
+              <canvas id="myChart" width="400" height="400"></canvas>
+              <script>
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Q1  2018', 'Q2 2018', 'Q3 2018', 'Q4 2018'],
+                        datasets: [{
+                          
+                          minBarLength: 9,
+                            label: 'Exit',
+                            data: [300000, 370000, 370000, 600000],
+                            backgroundColor: [
+                              // RGBA(253,6,6,1)
+                                'rgba(256, 006, 006, 1)',
+                                'rgba(256, 006, 006, 1)',
+                                'rgba(256, 006, 006, 1)',
+                                'rgba(256, 006, 006, 1)'
+                                // 'rgba(256, 46, 36, 1)'
+                              
+                            ],
+                        
+                            borderWidth: 1
+                        },
+                        {
+                          
+                          minBarLength: 9,
+                            label: 'Investment',
+                            data: [100000, 400000, 440000, 780000],
+                            backgroundColor: [
+                                'rgba(041,047, 089, 1)',
+                                'rgba(041,047, 089, 1)',
+                                'rgba(041,047, 089, 1)',
+                                'rgba(041,047, 089, 1)',
+                              
+                            ],
+              
+                            borderWidth: 1
+                        }
+                      ]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true,
+                                    stacked: true
+                                }
+                            }]
+                        }
+                    }
+                });
+                </script>
+            </div>
+          </div>
+          <div class="col-5 ml-5">
+            <div class="row ml-1 mt-5">
+              <p>TVPI</p>
+              <i class="fas fa-chevron-down ml-auto"> </i>
+            </div>
+            <canvas id="line-chart" height="400" width="400"> </canvas>
+            <script> 
+            let ctxc = document.getElementById('line-chart').getContext('2d');
+
+              let lineChart = new Chart(ctxc, {
+                type: 'line',
+                data: {
+                  
+                  labels: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+                  datasets: [{
+                    lineTension: 0,
+                    label: 'TVPI',
+                    data: [0.5, 2, 2.5, 1, 2.5, 3.5, 2, 3.7, 4, 4],
+                    backgroundColor: [
+                      // RGBA(122,239,31,1)
+                      'rgba(255,255, 255, 1)',
+                  ],
+                  }]
+                },
+                options: {
+                  bezierCurve: false
+                }
+              })
+              </script>
+          </div>
+        </div>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+      
+    </main>
 </body>
 </html>
